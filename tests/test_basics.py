94b0e717da15bd0b3b1632ff41c51e741805ef79
joppich <lejay@physik.tu-berlin.de>
2017-03-14 20:17:08 +0100

    using coverage

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,6 +2,7 @@ import unittest
 from flask import current_app
 from app import create_app, db
 
+
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
